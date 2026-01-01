ec77d3a234f1505a27e6187dbf1f65157ede4578
Tiago Rodrigues <tr364634@gmail.com>
2026-01-01 17:15:29 -0300

    poltergeist

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { AuthProvider } from './context/AuthContext';
@@ -34,49 +34,68 @@ const theme = createTheme({
   },
 });
 
-const App: React.FC = () => {
+function AppContent() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Detectar se veio de um redirecionamento 404
+    const params = new URLSearchParams(window.location.search);
+    const redirectedFrom = params.get('redirected');
+    
+    if (redirectedFrom && redirectedFrom !== location.pathname) {
+      // Redirecionar para a rota correta
+      window.history.replaceState({}, '', redirectedFrom);
+    }
+  }, [location]);
+
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
+      <Route 
+        path="/*" 
+        element={
+          <PrivateRoute>
+            <MainLayout>
+              <Routes>
+                <Route path="/" element={<DashboardCompleto />} />
+                <Route path="/dashboard" element={<DashboardCompleto />} />
+                <Route path="/contas-pagar" element={<ContasPagar />} />
+                <Route path="/fornecedores" element={<Fornecedores />} />
+                <Route path="/gastos-diarios" element={<GastosDiarios />} />
+                <Route path="/contas-bancarias" element={<ContasBancarias />} />
+                <Route path="/controle-contas" element={<ControleContas />} />
+                <Route path="/extrato" element={<Extrato />} />
+                <Route path="/perfil" element={<Perfil />} />
+                <Route path="/formas-pagamento" element={<FormasPagamento />} />
+                <Route path="/cartoes" element={<Cartoes />} />
+                <Route path="/notificacoes" element={<Notificacoes />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </Routes>
+            </MainLayout>
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  );
+}
+
+function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <AuthProvider>
         <Router>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/reset-password/:token" element={<ResetPassword />} />
-            <Route
-              path="/*"
-              element={
-                <PrivateRoute>
-                  <MainLayout>
-                    <Routes>
-                      <Route path="/" element={<DashboardCompleto />} />
-                      <Route path="/dashboard" element={<DashboardCompleto />} />
-                      <Route path="/contas-pagar" element={<ContasPagar />} />
-                      <Route path="/fornecedores" element={<Fornecedores />} />
-                      <Route path="/gastos-diarios" element={<GastosDiarios />} />
-                      <Route path="/contas-bancarias" element={<ContasBancarias />} />
-                      <Route path="/controle-contas" element={<ControleContas />} />
-                      <Route path="/extrato" element={<Extrato />} />
-                      <Route path="/perfil" element={<Perfil />} />
-                      <Route path="/formas-pagamento" element={<FormasPagamento />} />
-                      <Route path="/cartoes" element={<Cartoes />} />
-                      <Route path="/notificacoes" element={<Notificacoes />} />
-                      <Route path="*" element={<Navigate to="/" replace />} />
-                    </Routes>
-                  </MainLayout>
-                </PrivateRoute>
-              }
-            />
-          </Routes>
+          <AppContent />
         </Router>
       </AuthProvider>
       <PWAInstallPrompt />
       <PWAInstallBanner />
     </ThemeProvider>
   );
-};
+}
 
 export default App;
 
