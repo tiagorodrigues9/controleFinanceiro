5e6990720cbe7d55e4e97920236c065c74a7e62c
Tiago Rodrigues <tr364634@gmail.com>
2026-01-21 16:07:23 -0300

    testes

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse, AxiosError } from 'axios';
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api`,
+  baseURL: `${process.env.REACT_APP_API_URL || 'http://localhost:5000'}api`,
 });
 
 // Interceptor para adicionar token de autenticação
